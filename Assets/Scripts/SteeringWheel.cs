3d3af7dc5782481be37c965610496354e2bb1661
JoshDuffetyWong <josh.duffety-wong@grv.media>
2022-06-01 00:02:13 +0100

    Scriptable Objects for Events Harvick Event data HUD rework

---
+++ Assets/Scripts/SteeringWheel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnMouseDrag(){
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+ 
+        transform.position = objPosition;
+    }
+}
