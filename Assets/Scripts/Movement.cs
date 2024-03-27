3c45816e49d4ef27f0171991d11ba453be62ba6c
JoshDuffetyWong <josh@duffety-wong.com>
2024-03-27 00:27:29 +0000

    Indycar wreck speed equalised

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1395,8 +1395,12 @@ public class Movement : MonoBehaviour {
 		playerWreckDecel = baseDecel - (60f * wreckSine);
 		//Debug.Log(playerWreckDecel);
 		
+		if(seriesSpeedDiff > 0){
+			seriesSpeedDiff -= 1;
+		}
+
 		//Debug.Log("Wreck Decel: " + playerWreckDecel);
-		if((playerSpeed - speedOffset - CameraRotate.carSpeedOffset) + windForce <= 0){
+		if((playerSpeed - speedOffset - seriesSpeedDiff - CameraRotate.carSpeedOffset) + windForce <= 0){
 			endWreck();
 		}
 		
